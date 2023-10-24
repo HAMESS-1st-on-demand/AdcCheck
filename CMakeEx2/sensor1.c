0d69fc7555b4a57480634ef2d46b1480e47dcc8f
kimgiwon <kimgiwon0712@gmail.com>
2023-10-24 11:02:19 +0900

    필요없는 코드 정리

--- CMakeEx2/sensor1.c
+++ CMakeEx2/sensor1.c
@@ -14,15 +14,11 @@ int readADC(int channel){ // 지정된 SPI 채널에서 ADC 값을 읽습니다.
     buffer[0] = 1; // 채널에 따른 시작 바이트 설정
     buffer[1] = (0b10+channel)<<6 | 1<<5;
     buffer[2] = 0;
-    // buffer[0] = 0b110+channel; // 채널에 따른 시작 바이트 설정
-    // buffer[1] = 0;
-    // buffer[2] = 0;
+
     //printf("Send   : %x %x %x\n",buffer[0],buffer[1],buffer[2]);
-    
     digitalWrite(CS, LOW); // CS/SHDN bit을 0으로 만들어야 값을 얻을 수 있다.
     wiringPiSPIDataRW(0, buffer, 3); // SPI 장치와 데이터 교환
     digitalWrite(CS, HIGH); // CS/SHDN bit : CHANNEL 바뀔 때 1로 setting
-
     //printf("Recieve: %x %x %x\n",buffer[0],buffer[1],buffer[2]);
     adc = ((buffer[1] & 0x0F) << 8) + buffer[2]; // 데이터 교환 후 ADC 값 버퍼 추출, 반환
     return adc;
@@ -41,8 +37,8 @@ int main(void){
     pinMode(CS, OUTPUT);
 
     while(1){
-        // int adcValue1 = readADC(ADC_CHANNEL1);
-        // printf("ADC value 1 = %d\n", adcValue1);
+        //int adcValue1 = readADC(ADC_CHANNEL1);
+        //printf("ADC value 1 = %d\n", adcValue1);
         int adcValue2 = readADC(ADC_CHANNEL2); 
         printf("ADC value 2 = %d\n", adcValue2);
         delay(1000);
